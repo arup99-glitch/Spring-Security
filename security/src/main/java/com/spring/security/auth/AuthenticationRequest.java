3dbced2383413c44926136b03a8fc3e25540b942
arup99-glitch <arupchakraborty153@gmail.com>
2024-02-21 04:13:34 +0600

    First Commit

---
+++ security/src/main/java/com/spring/security/auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.spring.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    String password;
+}
